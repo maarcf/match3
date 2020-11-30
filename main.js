157c694b918fff40f37d9935dc5d6d992030a46c
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-11-30 14:50:09 -0300

    modifico medidas para mejorar el maquetado de l agrilla

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const frutas = ['🍉', '🥝', '🍌', '🍇', '🍋', '🥥'];
 
 let columnas = 9 
 let anchoDeDiv = 0
-const anchoDeGrilla = 500
+const anchoDeGrilla = 480
 let dificultad = ""
 let grillaJS = [];  
 
